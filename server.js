2ed8784478769d985a75ae6b73935a8299e578fd
Matt Creager <mattcreager@goinstant.com>
2013-10-24 09:21:50 -0300

    Which port am I on?

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
+var port = process.env.PORT || 5000;
+
 require('harp')
-  .server(__dirname, { port: process.env.PORT || 5000 });
+  .server(__dirname, { port: port });
+
+console.log('HARP is serving cotent on port: ' + port);
