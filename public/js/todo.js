62ac4f8ebcd7c60daa1c4fda87bd0706531a9d13
Matthew Creager <mattcreager@goinstant.com>
2014-02-07 09:08:49 -0400

    feat(query): Adding filter queries

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,25 +1,67 @@
+/* global _ , angular*/
+
 'use strict';
+
 angular
-  .module('todoApp', ['goinstant'])
-  .config(function(platformProvider) {
-    platformProvider.set('https://goinstant.net/mattcreager/DingDong');
+  .module('todoApp', ['goangular'])
+  .config(function($goConnectionProvider) {
+    $goConnectionProvider.$set('https://goinstant.net/mattcreager/DingDong');
   })
-  .controller('TodoCtrl', function($scope, GoAngular) {
+  .controller('TodoCtrl', ['$scope', '$goQuery', function($scope, $query) {
 
-    // Init GoAngular
-    new GoAngular($scope, 'BinDing', { include: ['todos'] }).initialize();
+    $scope.addTodo = _.throttle(function() {
+      var desc = $scope.newTodo;
 
-    $scope.todos = [];
+      if (desc === undefined || desc === '') {
+        return;
+      }
 
-    $scope.addTodo = function() {
-      $scope.todos.push({
-        description: $scope.newTodo,
+      $scope.todos.$add({
+        timestamp: new Date().getTime(),
+        description: desc,
         complete: false
+      }).then(function() {
+        $scope.$apply(function() {
+          $scope.newTodo = '';
+        });
+      });
+    }, 100);
+
+    $scope.changeStatus = function(key) {
+      var status = !$scope.todos[key].complete;
+
+      $scope.todos[key].pendingUpdate = true;
+      $scope.todos.$key(key + '/complete').$set(status);
+    };
+
+    $scope.removeTodo = function(key) {
+      $scope.todos.$key(key).$remove();
+    };
+
+    $scope.showAll = function() {
+      $scope.todos = $query('todos', {});
+      $scope.todos.$sync();
+    };
+
+    $scope.showActive = function() {
+      $scope.todos = $query('todos', { complete: false }, {});
+      $scope.todos.$sync();
+    };
+
+    $scope.showComplete = function() {
+      $scope.todos = $query('todos', { complete: true }, {});
+      $scope.todos.$sync();
+    };
+
+    $scope.clearCompleted = function() {
+      var completeTodos = $query('todos', { complete: true }, {}).$sync();
+
+      completeTodos.$on('ready', function() {
+         _.map(completeTodos.$omit(), function(todo, key) {
+          completeTodos.$key(key).$remove();
+        });
       });
     };
 
-    $scope.removeTodo = function(todo) {
-      $scope.todos.splice($scope.todos.indexOf(todo), 1);
-    };
-
-  });
+    $scope.showAll();
+  }]);
